c1fa972c35758b6a3c5b167486ddba2b1142d9ff
novicailievski93 <novica.ilievski@intertec.io>
2018-05-07 16:35:55 +0200

    Added new lines

--- src/kumanovo.js
+++ src/kumanovo.js
@@ -9,14 +9,21 @@
      * @returns {*}
      */
     function isPrime(value) {
-        if (!isPrime.answers) isPrime.answers = {};
+        if (!isPrime.answers) 
+            isPrime.answers = {};
+        
         if (isPrime.answers[value] != null) {
             return isPrime.answers[value];
         }
         var prime = value != 1; // 1 can never be prime
+
+
+
+
+
         var limit = Math.sqrt(value) + 1 // number is prime if no divisor is found in this limit
         for (var i = 2; i < limit; i++) {
-            
+
             if (value % i == 0) {
                 prime = false;
                 break;
